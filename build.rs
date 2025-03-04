63936ca7b4fe120fa434fc8e9034a879f543b9a3
WangWindow <1598593280@qq.com>
2025-03-04 21:42:21 +0800

    Update: Add `Iced` UI Library and CN fonts support.
    添加窗口图标和字体支持，创建一个用于测试UI界面的计数器应用程序。

---
+++ build.rs
@@ -0,0 +1,18 @@
+/*
+ * @FilePath: build.rs
+ * @Author: WangWindow 1598593280@qq.com
+ * @Date: 2025-03-04 21:13:22
+ * @LastEditors: WangWindow
+ * @LastEditTime: 2025-03-04 21:13:30
+ * 2025 by WangWindow, All Rights Reserved.
+ * @Description:
+ */
+extern crate winres;
+
+fn main() {
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("res/icons/bingtang.ico");
+        res.compile().unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,67 @@
  * @Author: WangWindow 1598593280@qq.com
  * @Date: 2025-03-04 19:33:15
  * @LastEditors: WangWindow
- * @LastEditTime: 2025-03-04 19:34:04
+ * @LastEditTime: 2025-03-04 21:05:25
  * 2025 by WangWindow, All Rights Reserved.
  * @Description:
  */
-fn main() {
-    println!("Hello, world!");
+use iced::{
+    Font,
+    widget::{Column, button, column, text},
+    window::{Icon, Settings, icon},
+};
+
+fn main() -> iced::Result {
+    iced::application("计数器", Counter::update, Counter::view)
+        .font(include_bytes!("../res/fonts/SourceHanSansCN-Regular.otf")) // 载入字体文件
+        .default_font(Font::with_name("思源黑体 CN")) // 设置默认字体
+        .window(Settings {
+            icon: Some(Icon::from(
+                icon::from_file_data(include_bytes!("../res/icons/bingtang.ico"), None).unwrap(),
+            )), // 设置窗口 icon
+            ..Default::default() // 窗口的其余设置用默认
+        })
+        .run()
+}
+
+#[derive(Default)]
+struct Counter {
+    value: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    Increment,
+    Decrement,
+}
+
+impl Counter {
+    pub fn view(&self) -> Column<Message> {
+        // We use a column: a simple vertical layout
+        column![
+            // The increment button. We tell it to produce an
+            // `Increment` message when pressed
+            button("增加").on_press(Message::Increment),
+            // We show the value of the counter here
+            text(self.value).size(50),
+            // The decrement button. We tell it to produce a
+            // `Decrement` message when pressed
+            button("减少").on_press(Message::Decrement),
+        ]
+    }
+}
+
+impl Counter {
+    // ...
+
+    pub fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => {
+                self.value += 1;
+            }
+            Message::Decrement => {
+                self.value -= 1;
+            }
+        }
+    }
 }

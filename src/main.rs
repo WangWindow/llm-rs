a1c01b388421f7893f789da35dd5eaf64cc3c14d
WangWindow <1598593280@qq.com>
2025-03-04 19:35:31 +0800

    Init: Create project.
    初始化项目结构，添加 .gitignore、Cargo.toml 和 Cargo.lock 文件，并创建主程序文件 main.rs

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+/*
+ * @FilePath: main.rs
+ * @Author: WangWindow 1598593280@qq.com
+ * @Date: 2025-03-04 19:33:15
+ * @LastEditors: WangWindow
+ * @LastEditTime: 2025-03-04 19:34:04
+ * 2025 by WangWindow, All Rights Reserved.
+ * @Description:
+ */
+fn main() {
+    println!("Hello, world!");
+}

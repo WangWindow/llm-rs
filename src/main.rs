bdb4809d4f0dd0de6665d88ff0efeb5a5e0c3269
WangWindow <1598593280@qq.com>
2025-03-05 20:10:18 +0800

    Update: LLM agent(using `rig` crate) and add new dependencies.

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,45 @@
  * @Author: WangWindow 1598593280@qq.com
  * @Date: 2025-03-04 19:33:15
  * @LastEditors: WangWindow
- * @LastEditTime: 2025-03-04 23:34:28
+ * @LastEditTime: 2025-03-05 19:55:30
  * 2025 by WangWindow, All Rights Reserved.
- * @Description:
+ * @Description: A simple LLM agent, using rig(API) and iced(UI).
  */
 use iced::{
     Font,
     widget::{Column, button, column, text},
     window::{Icon, Settings, icon},
 };
+use rig::{agent::AgentBuilder, completion::Prompt, providers::deepseek};
 
-fn main() -> iced::Result {
-    iced::application("计数器", Counter::update, Counter::view)
-        .font(include_bytes!("../res/fonts/SourceHanSansCN-Regular.otf")) // 载入字体文件
-        .default_font(Font::with_name("思源黑体 CN")) // 设置默认字体
+#[tokio::main]
+async fn main() -> iced::Result {
+    // 创建 LLM 客户端
+    let client = deepseek::Client::from_env();
+    let deepseek = client.completion_model(deepseek::DEEPSEEK_CHAT);
+    let agent = AgentBuilder::new(deepseek)
+        .preamble(
+            "\
+        You are Gandalf the white and you will be conversing with other \
+        powerful beings to discuss the fate of Middle Earth.\
+    ",
+        )
+        .build();
+    let response = agent
+        .prompt("What is the best way to defeat Sauron?")
+        .await
+        .unwrap();
+    println!("{response}");
+
+    // 创建应用程序
+    iced::application("LLM Agent", Counter::update, Counter::view)
+        .font(include_bytes!("../res/fonts/SourceHanSansCN-Regular.otf"))
+        .default_font(Font::with_name("思源黑体 CN"))
         .window(Settings {
             icon: Some(Icon::from(
                 icon::from_file_data(include_bytes!("../res/icons/bingtang.ico"), None).unwrap(),
-            )), // 设置窗口 icon
-            ..Default::default() // 窗口的其余设置用默认
+            )),
+            ..Default::default()
         })
         .run()
 }
@@ -39,22 +59,12 @@ pub enum Message {
 
 impl Counter {
     pub fn view(&self) -> Column<Message> {
-        // We use a column: a simple vertical layout
         column![
-            // The increment button. We tell it to produce an
-            // `Increment` message when pressed
             button("增加").on_press(Message::Increment),
-            // We show the value of the counter here
             text(self.value).size(50),
-            // The decrement button. We tell it to produce a
-            // `Decrement` message when pressed
             button("减少").on_press(Message::Decrement),
         ]
     }
-}
-
-impl Counter {
-    // ...
 
     pub fn update(&mut self, message: Message) {
         match message {
